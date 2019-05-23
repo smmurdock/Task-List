c9fb2ced1e411e4966b32d2ecc804ecaca79e4ad
Shanay Murdock <murdock.shanay@gmail.com>
2019-05-22 21:29:34 -0400

    Delete and filter tasks

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+  // Clear task event
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter through task event
+  filter.addEventListener("keyup", filterTasks);
 }
 
 // Add Task
@@ -42,4 +48,41 @@ function addTask(e) {
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+
+    // console.log(e.target);
+  }
+}
+
+// Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = "";
+
+  // Faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // https://jsperf.com/innerhtml-vs-removechild
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
